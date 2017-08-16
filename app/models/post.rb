68c89904f8d68e4e4183aaf1c120b552231189b7
Hui Wang <huiw3939@gmail.com>
2017-08-16 11:05:45 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 100}
+  validates :summary, length: {maximum: 299}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+  validate :click_bait
+
+  def click_bait
+    if title != nil && !title.start_with?('Won\'t Believe', 'Secret', 'Top', 'Guess', "You Won\'t")
+      errors.add(:title, "Not clickbait title")
+  end
+end
+
 end
